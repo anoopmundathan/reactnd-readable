36633e64f8b17bd45a41c5f74dee323397b30927
Anoop Mundathan <anoop.mundathan@gmail.com>
2017-08-25 22:35:51 +0100

    Move fetch categories out from App

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,16 @@
 import React, { Component } from 'react'
 import { Link, Route } from 'react-router-dom'
-import Posts from './Posts'
+
 import Categories from './Categories'
+import Posts from './Posts'
 import Comments from './Comments'
 
-import { getAllCategories, getAllPostsForCategory } from '../utils/ReadableAPI'
 import '../App.css'
 
 class App extends Component {
-  
-  state = {
-    categories: []
-  }
-
-  componentDidMount() {  
-    getAllCategories()
-      .then(categories => {
-        const items = categories.map(item => item.name)
-        this.setState({ categories: items })
-      })
-  }
 
   render() {
-    const { categories } = this.state
+
     return(
       <div>
         <div>
@@ -30,17 +18,9 @@ class App extends Component {
           <Link to='/posts'>Posts</Link>
           <Link to='/comments'>Comments</Link>
         </div>
-        <Route path ='/posts' component={Posts} />
-        <Route path ='/comments' component={Comments} />
-        <Route path='/categories' render={() => (
-          <Categories 
-            categories={categories} 
-            onNavigate={() => {
-              this.setState({
-                screen: 'view'
-              })
-            }}/>
-        )} />
+          <Route path ='/posts' component={Posts} />
+          <Route path ='/comments' component={Comments} />
+          <Route path ='/categories' component={Categories} />
       </div>
     )
   }
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
+import { getAllCategories } from '../utils/ReadableAPI'
 
 class Categories extends Component {
+  
+  state = {
+    categories: []
+  }
+
+  componentDidMount() {  
+    getAllCategories()
+      .then(categories => {
+        const items = categories.map(item => item.name)
+        this.setState({ categories: items })
+      })
+  }
 
   render() {    
-    const { categories } = this.props
+    const { categories } = this.state
     const list = categories.map((item, index) => <li key={index}>{item}</li>)
     return(
       <div>
