4bdd80c4e414c9de3c1c3cbe8378610cb5f25c53
Anoop Mundathan <anoop.mundathan@gmail.com>
2017-10-05 07:34:38 +0100

    Delete Comment

--- src/components/PostDetail/CommentList.js
+++ src/components/PostDetail/CommentList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { deleteComment } from '../../utils/ReadableAPI'
 
 const CommentBody = (props) => {
   return(
@@ -8,20 +9,34 @@ const CommentBody = (props) => {
   )
 }
 
-const CommentEdit = () => {
+const CommentEdit = (props) => {
   return(
     <div className="Edit">
-      <input type="button" value="Edit" />
+      <input 
+        onClick={props.onEdit}
+        type="button" 
+        value="Edit" />
     </div>
   )
 }
 
-const CommentDelete = () => {
-  return(
-    <div className="Delete">
-      <input type="button" value="Delete"/>
+class CommentDelete extends Component {
+
+  onDelete = () => {
+    const id = this.props.id
+    this.props.onDelete(id) 
+  }
+
+  render() {
+    return(
+      <div className="Delete">
+      <input
+        onClick={this.onDelete} 
+        type="button" 
+        value="Delete"/>
     </div>
-  )
+    )
+  }
 }
 
 const CommentContainer = (props) => {
@@ -29,14 +44,23 @@ const CommentContainer = (props) => {
     <div className="Comment-Container">
       <CommentBody body={props.body}/>
       <div className="Comment-Edit-Delete">
-        <CommentEdit />  
-        <CommentDelete />  
+        <CommentEdit id={props.id} onEdit={props.onEdit}/>  
+        <CommentDelete id={props.id} onDelete={props.onDelete}/>  
       </div>
     </div>
   )
 }
 
 export class CommentList extends Component {
+
+  onDelete = (id) => {
+    deleteComment(id)
+  }
+
+  onEdit = (id) => {
+    console.log('edit')
+  }
+
   render() {
 
     let commentList = null
@@ -46,7 +70,11 @@ export class CommentList extends Component {
         <li 
           className="Comment"
           key={comment.id}>
-          <CommentContainer body={comment.body}/>
+          <CommentContainer
+            onDelete={this.onDelete}
+            onEdit={this.onEdit}
+            id={comment.id}
+            body={comment.body}/>
         </li>
       ))
     }
